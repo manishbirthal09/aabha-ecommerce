c6d1f936f00c14c14c5052371015a45005a363db
manish birthal <manishbirthal4717@gmail.com>
2026-08-30 12:13:19 +0530

    fix import paths in Layout.jsx

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,6 +1,6 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import AnnouncementBar from "./components/AnnouncementBar";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import AnnouncementBar from "./AnnouncementBar";
 import { Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 export default function Layout() {
