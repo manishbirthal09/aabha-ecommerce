50fa5b08ae685e8600880621e9d5fce6699ef4ba
manish birthal <manishbirthal4717@gmail.com>
2026-08-30 11:57:41 +0530

    add navbar and footer to all pages

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import AnnouncementBar from "../components/AnnouncementBar";
+import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+export default function Layout() {
+     const location = useLocation();
+  const isHome = location.pathname === "/";
+  return (
+    <>
+    {isHome && <AnnouncementBar />}
+      <Navbar />
+      <main className={isHome ? "pt-24 md:pt-28" : "pt-16 md:pt-20"}>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
