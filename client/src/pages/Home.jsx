d39554d2c55d542ccc9e255ed3432bbd72a17be5
manish birthal <manishbirthal4717@gmail.com>
2026-08-30 12:16:40 +0530

    remove duplicate navbar/footer from Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import AnnouncementBar from '../components/AnnouncementBar';
-import Navbar from '../components/Navbar';
+
 import Hero from '../components/Hero';
 import TrendingNow from '../components/TrendingNow';
 import InstagramSection from '../components/InstagramSection';
-import Footer from '../components/Footer';
+
 import About from '../components/About';
 import CustomCandleBanner from '../components/CustomCandleBanner';
 import USPSlider from '../components/USPSection';
@@ -12,8 +11,7 @@ import CategoryCircles from '../components/CategoryCircles';
 const Home = () => {
   return (
     <div>
-      <AnnouncementBar />
-      <Navbar />
+      
       <Hero />
       <CategoryCircles />
       <TrendingNow />
