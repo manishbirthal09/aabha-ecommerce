3f7a14a1ceb4fe59c5fa41c068412c5443c39c30
manish birthal <manishbirthal4717@gmail.com>
2026-09-12 17:29:39 +0530

    remove bogo

--- server/utils/calculateBogo.js
+++ server/utils/calculateBogo.js
@@ -1,23 +1,23 @@
 
-export function calculateBogoDiscount(cartItems, bogoEnabled) {
-  if (!bogoEnabled) return 0;
+// export function calculateBogoDiscount(cartItems, bogoEnabled) {
+//   if (!bogoEnabled) return 0;
 
-  const allPrices = [];
-  cartItems.forEach((item) => {
-    const price = item.product.discountPrice || item.product.price;
-    for (let i = 0; i < item.quantity; i++) {
-      allPrices.push(price);
-    }
-  });
+//   const allPrices = [];
+//   cartItems.forEach((item) => {
+//     const price = item.product.discountPrice || item.product.price;
+//     for (let i = 0; i < item.quantity; i++) {
+//       allPrices.push(price);
+//     }
+//   });
 
-  allPrices.sort((a, b) => a - b);
+//   allPrices.sort((a, b) => a - b);
 
-  const totalItems = allPrices.length;
-  const freeItemsCount = Math.floor(totalItems / 3);
+//   const totalItems = allPrices.length;
+//   const freeItemsCount = Math.floor(totalItems / 3);
 
-  let discount = 0;
-  for (let i = 0; i < freeItemsCount; i++) {
-    discount += allPrices[i];
-  }
-  return discount;
-}
+//   let discount = 0;
+//   for (let i = 0; i < freeItemsCount; i++) {
+//     discount += allPrices[i];
+//   }
+//   return discount;
+// }
